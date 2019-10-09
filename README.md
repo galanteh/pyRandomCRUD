6a52013fb96aa1a29699334112f549cfd2bb143d
Hernan Galante <hernan_galante@hotmail.com>
2019-10-08 19:43:30 -0500

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# pyRandomCRUD
-Python script that insert, update or delete rows into a table over ODBC
+# Random ops
+Python script that makes a lot of random operations over a simple table. 
+In our case, the table is Customers with the following definition:
+
+```sql 
+CREATE TABLE customers 
+    (id INTEGER IDENTITY(1001,1) NOT NULL PRIMARY KEY, 
+    first_name VARCHAR(255) NOT NULL, 
+    last_name VARCHAR(255) NOT NULL, 
+    email VARCHAR(255) NOT NULL UNIQUE)
+```
+
+## Requeriments
+This python script will need: 
+* pyodbc. This library required unixodbc. On Mac, just use brew (brew install unixodbc)
+* faker. Library that generates random first, last and email addresses in the language you require.
+
+## UnixODBC
+Once you have installed unixOdbc on  
+```bash
+$ odbcinst -j
+unixODBC 2.3.7
+DRIVERS............: /usr/local/etc/odbcinst.ini
+SYSTEM DATA SOURCES: /usr/local/etc/odbc.ini
+FILE DATA SOURCES..: /usr/local/etc/ODBCDataSources
+USER DATA SOURCES..: /Users/hgalante/.odbc.ini
+SQLULEN Size.......: 8
+SQLLEN Size........: 8
+SQLSETPOSIROW Size.: 8
+```
+You need to download the SQLServer driver. In order to do that, please refer to this URL and look for the steps of your platform:
+
+* [https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-2017]()
+
+### Test UnixODBC
+
+```bash
+sqlcmd -S<server_name> -U<user> -P<passowrd>
+```
+
+## 
---
+++ pyRandomCRUD.py
@@ -0,0 +1,91 @@
+import pyodbc
+import random
+from faker import Faker
+from random import randrange
+
+class RandomCRUD:
+
+    def __init__(self, language = 'es_MX'):
+        self.fake = Faker(language)
+        self.driver_name = '{ODBC Driver 13 for SQL Server}'
+        self.server_name = 'hgalante-sqlserver-serpro.gce.cloudera.com'
+        self.db_user_name= 'sa'
+        self.db_password = 'SQL4512!'
+        self.db_name = 'CDC_SAMPLE'
+        self.column_name_list = ['first_name', 'last_name', 'email']
+        self.conn = None
+        self._set_connection_database()
+
+    def _set_connection_database(self):
+        connection_string = 'Driver={0};SERVER={1};DATABASE={2};UID={3};PWD={4};'
+        connection_string = connection_string.format(self.driver_name,self.server_name, self.db_name, self.db_user_name, self.db_password)
+        self.conn = pyodbc.connect(connection_string)
+
+    def get_unique_email(self):
+        cursor = self.conn.cursor()
+        email = self.fake.email()
+        sql = "SELECT COUNT(*) FROM CDC_SAMPLE.dbo.customers WHERE email = '{0}'".format(email)
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        if len(rows) > 0:
+            if rows[0][0] > 0:
+                return self.get_unique_email()
+            else:
+                return email
+        else:
+            return None
+
+    def do_random_insert(self):
+        cursor = self.conn.cursor()
+        sql = "INSERT INTO Customers(first_name, last_name, email) values ('{0}', '{1}', '{2}')"
+        sql = sql.format(self.fake.first_name(), self.fake.last_name(), self.get_unique_email())
+        cursor.execute(sql)
+        self.conn.commit()
+
+    def do_random_delete(self):
+        row_id = self.get_random_row_id()
+        if row_id is None:
+            return
+        cursor = self.conn.cursor()
+        sql = "DELETE FROM Customers WHERE ID = '{0}'"
+        cursor.execute(sql.format(row_id))
+        self.conn.commit()
+
+    def do_random_update(self):
+        row_id = self.get_random_row_id()
+        if row_id is None:
+            return
+        column_name = random.choice(self.column_name_list)
+        new_value = ''
+        new_value = getattr(self.fake, column_name)()
+        cursor = self.conn.cursor()
+        sql = "UPDATE Customers SET {0} = '{1}' WHERE ID = '{2}'"
+        cursor.execute(sql.format(column_name, new_value, row_id))
+        self.conn.commit()
+
+    def get_random_row_id(self):
+        cursor = self.conn.cursor()
+        sql = "SELECT TOP 1 ID FROM CDC_SAMPLE.dbo.customers ORDER BY NEWID();"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        if len(rows) > 0:
+            return rows[0][0]
+        else:
+            return None
+
+    def run_operations(self, times = 1):
+        for index in range(1, times):
+            operation = randrange(2)
+            if operation == 0: #insert
+                print('Running a random insert operation')
+                self.do_random_insert()
+            if operation == 1:  # update
+                print('Running a random update operation')
+                self.do_random_update()
+            if operation == 2:  # delete
+                print('Running a random delete operation')
+                self.do_random_delete()
+
+if __name__ == '__main__':
+    rop = RandomCRUD()
+    rop.run_operations(10)
